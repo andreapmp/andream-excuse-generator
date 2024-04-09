d63a0d637144a26924f7d7ae63802d2d33fcac9b
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-04-09 03:54:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,48 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // 1. Create the "parts of the sentence" arrays
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "The bird",
+    "The cat",
+    "My mom",
+    "The squirrel"
+  ];
+  let action = ["ate", "threw away", "mailed", "stole", "lost", "broke"];
+  let what = [
+    "my homework",
+    "my cellphone",
+    "the car",
+    "my project",
+    "my presentation"
+  ];
+  let when = [
+    "yesterday!",
+    "this morning!",
+    "last week",
+    "last night",
+    "the other day"
+  ];
+
+  // 2. combine random elements in each array to form a sentence
+  function selectArrayElement(arrayName, arrayLength) {
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  //3 print the excuse on the terminal and in the browser
+  const htmlElement = document.querySelector(".excuse");
+  htmlElement.innerHTML = excuse;
 };
